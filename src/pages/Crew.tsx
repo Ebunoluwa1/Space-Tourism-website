fd98c07495883c07f6f951aa099e62cdaf8e744f
Ebunoluwa Oni <ebuntoday@gmail.com>
2025-10-21 12:52:38 +0100

    crew and destination done

--- src/pages/Crew.tsx
+++ src/pages/Crew.tsx
@@ -1,12 +1,142 @@
 
-import Layout from '../components/Layout';
+    import { Flex, Text, Heading, VStack,Image, Box, HStack, Circle } from '@chakra-ui/react';
+    import MobileCrewImg from '../assets/crew/background-crew-mobile.jpg'
+    import TabCrewImg from '../assets/crew/background-crew-tablet.jpg'
+    import DeskCrewImg from '../assets/crew/background-crew-desktop.jpg'
+    import Layout from '../components/Layout';
+    import { useState } from 'react';
+    import data from '../data/data.json'
+     import { motion } from 'framer-motion';
 
-const Crew = () => {
-  return (
+    const Crew = () => {
+
+    const crews = data.crew;
+    const [crewIndex, setCrewIndex] = useState(0);
+    const active = crews[crewIndex];
+
+    const MotionBox = motion(Box);
+     const MotionImage = motion(Image);
+    
+    return (
     <Layout>
-      yup
-    </Layout>
-  );
-}
+        <Flex
+        as={'section'} 
+        bgImage={{
+        base: `url(${MobileCrewImg})`,
+        md: `url(${TabCrewImg})`,
+        lg: `url(${DeskCrewImg})`
+        }}
+        bgSize={'cover'}
+        bgPos={'center'}
+        align={'center'}
+        justify={'space-between'}
+        direction={{base: 'column', lg:'row'}}
+        textAlign= {{base: 'center', md: 'center', lg:'left'}}
+        overflow={'hidden'}
+        position={'relative'}
+        px={{base: '6',md:'16', lg: '24'}}
+        py={{base: '8',md: '20', lg:'28'}}
+        >
+        {/* Left content */}
+        <VStack
+              align={{ base: "center", lg: "flex-start" }}
+              textAlign={{ base: "center", lg: "left" }}
+              flex='1'
+              spacing={8}>
+                <Heading
+                as='h2'
+                color="white"
+                textTransform="uppercase"
 
-export default Crew;
+                fontSize={{ base: "xl", md: "2xl", lg: "3xl" }}
+                letterSpacing="2px"
+                >
+                <Text 
+                as={'span'}
+                color='gray.500'
+                mr='4px'>
+                02
+                </Text> 
+                Meet your Crew
+                </Heading>
+
+              {/* left content content */}
+
+              <MotionBox
+                  key={active.name}
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.8 }}>
+                      <Text
+                      fontFamily={'h3'}
+                        fontSize={{ base: "md", md: "lg", lg: "xl" }}
+                        color="gray.500"
+                        textTransform="uppercase"
+                      >
+                        {active.role}
+                      </Text>
+
+                      <Heading
+                        as="h1"
+                        fontFamily="heading"
+                        fontWeight="normal"
+                        textTransform="uppercase"
+                        fontSize={{ base: "2xl", md: "4xl", lg: "5xl" }}
+                        mb={4}
+                      >
+                        {active.name}
+                      </Heading>
+
+                        <Text
+                        color="gray.300"
+                        fontFamily="body"
+                        lineHeight="tall"
+                        maxW="400px"
+                        mx={{ base: "auto", lg: "0" }}
+                        fontSize={{ base: "sm", md: "md" }}
+                      >
+                        {active.bio}
+                      </Text>
+              </MotionBox>
+
+
+        {/* dot navigation */}
+              <HStack 
+              spacing={4}
+              pt={{base: 4, lg: 6}}
+              justify={{base: 'center', lg: 'flex-start' }}>
+
+            {crews.map ((_,idx) => (
+              <Circle 
+              key={idx}
+              size={{base: '10px', md: '12px'}}
+              bg={idx === crewIndex ? 'white' : 'gray.500'}
+              cursor='pointer'
+              transition = 'all 0.3s ease'
+              _hover={{ opacity:0.8}}
+              opacity={idx === crewIndex ? 1 : 0.5}
+              onClick={() => setCrewIndex(idx)}
+              />
+            
+            ))}
+            </HStack>
+      </VStack>
+
+        {/* Right content */}
+          <MotionImage 
+            key={active.name}
+                    src={active.images.png.replace("./", "/")}
+                    alt={active.name}
+                    initial={{ opacity: 0, x: 50 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.8 }}
+                    w={{ base: "250px", md: "350px", lg: "450px" }}
+                    alignSelf={{ base: "center", lg: "flex-end" }}s
+          />
+
+        </Flex>
+    </Layout>
+    );
+    }
+
+    export default Crew;
--- src/pages/Destination.tsx
+++ src/pages/Destination.tsx
@@ -1,11 +1,21 @@
 
-import { Flex } from '@chakra-ui/react';
+import { Flex, Heading, VStack, Text, Image, Tabs, TabList, Tab, TabPanels, TabPanel, HStack } from '@chakra-ui/react';
 import Layout from '../components/Layout';
 import MobileDestImg from '../assets/destination/background-destination-mobile.jpg'
 import TabDestImg from '../assets/destination/background-destination-tablet.jpg'
 import deskDestImg from '../assets/destination/background-destination-desktop.jpg'
+import data from '../data/data.json'
+import { useState } from 'react';
+import { motion } from 'framer-motion';
 
 const Destination = () => {
+  const destinations = data.destinations;
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const active = destinations[activeIndex];
+
+  const MotionImage=motion(Image)
+
   return (
     <Layout>
        <Flex
@@ -27,15 +37,127 @@ const Destination = () => {
              py={{base: '8',md: '20', lg:'28'}}
              >
            {/* Left content */}
-               <Text 
-                       fontFamily={'subheading'}
-                       letterSpacing={'4px'}
+             <VStack flex='1' spacing={8}>
+                <Heading
+                 as='h2'
+                 color="white"
+                textTransform="uppercase"
+
+                 fontSize={{ base: "xl", md: "2xl", lg: "3xl" }}
+                letterSpacing="2px"
+                 >
+                   <Text 
+                   as='span' 
+                   color='gray.500'
+                    mr='4px'>
+                      01
+                    </Text> 
+                    Pick your destination
+                 </Heading>
+
+                 <MotionImage 
+                  src={active.images.png.replace('./', '/')}
+                  alt={active.name}
+                  boxSize={{base: '200px', md: '300px',lg:'400px'}}
+                  objectFit={'contain'}
+                  mx={'auto'}
+                    initial={{ opacity: 0, scale: 0.9 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{ duration: 0.5 }}
+                  />
+             </VStack>
+
+             {/* Right content */}
+             <VStack  
+             flex="1"
+              align={{ base: "center", lg: "flex-start" }}
+              spacing={6}
+              textAlign={{ base: "center", lg: "left" }}
+              mt={{ base: 5, lg: 0 }} >
+               <Tabs variant={'unstyled'}  onChange={(index) => setActiveIndex(index)} >
+                <TabList justifyContent={{base: 'center', lg:'flex-start'}}>
+                 {destinations.map((dest) => (
+                  <Tab key={dest.name}
+                  _selected={{
+                    color:'white',
+                    borderBottom: '3px solid white'
+                  }}
+                  color={'lightBlue'}
+                  textTransform={'uppercase'}
+                  fontFamily={'subheading'}
+                  fontSize={{base: 'sm', md: 'md'}}
+                  mx={2}
+                  >{dest.name}
+                  </Tab>
+                 ))}
+                </TabList>
+
+                 <TabPanels>
+                  {destinations.map((dest) => (
+                    <TabPanel key={dest.name}>
+                      <Heading
+                      as='h1'
+                      textTransform={'uppercase'}
+                      fontFamily={'heading'}
+                      fontWeight={'normal'}
+                      fontSize={{ base: '5xl', md:'6xl', lg:'7xl'}}
+                      >{dest.name}
+                      </Heading>
+
+                      <Text
                        color={'lightBlue'}
-                       fontSize={{ sm:'sm', md:'md',lg:'subheading2'}}
-                       textTransform={'uppercase'}
+                       fontFamily={'body'}
+                       maxW={{ base: 'sm',md:'lg'}}
+                       mt={4}>
+                        {dest.description}
+                      </Text>
+
+                      <HStack
+                       justify={{ base: 'center', lg: 'flex-start'}}
+                       spacing={10}
+                       mt={8}
+                       pt={6}
+                       borderTop={'1px solid rgba(255,255,255,0.2)'}
                        >
-                              So, you want to travel to
-                       </Text>
+                          <VStack spacing={1}>
+                             <Text color={'lightBlue'}
+                             textTransform={'uppercase'} 
+                             fontSize={'sm'} >
+                               Avg. Distance
+                             </Text>
+
+                             <Text 
+                             color='white'
+                             fontFamily={'heading'}
+                             fontSize={{ base: '2xl', md: '3xl'}}
+                             textTransform={'uppercase'}
+                             >
+                              {dest.distance}
+                             </Text>
+                          </VStack>
+
+                          <VStack spacing={1}>
+                            <Text color="lightBlue" textTransform="uppercase" fontSize="sm">
+                              Est. Travel Time
+                            </Text>
+                              <Text
+                                color="white"
+                                fontFamily="heading"
+                                fontSize={{ base: "2xl", lg: "3xl" }}
+                                textTransform="uppercase"
+                              >
+                                {dest.travel}
+                              </Text>
+
+                          </VStack>
+                      </HStack>
+
+                    </TabPanel>
+                  ))}
+                 </TabPanels>
+
+               </Tabs>
+             </VStack>
        </Flex>
     </Layout>
   );
